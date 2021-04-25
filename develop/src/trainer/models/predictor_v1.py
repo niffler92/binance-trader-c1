8dca27a9723c8b917e9474bc0cf74d3a6335935c
Lyu <app@live.jp>
2021-04-25 09:53:24 +0900

    Add binary focal loss

--- develop/src/trainer/models/predictor_v1.py
+++ develop/src/trainer/models/predictor_v1.py
@@ -103,7 +103,7 @@ class PredictorV1(BasicPredictor):
 
         # Y loss
         loss = self.criterion(pred_abs_factor, train_data_dict["Y"].view(-1).abs()) * 10
-        loss += self.binary_cross_entropy(
+        loss += self.binary_criterion(
             pred_sign_factor, (train_data_dict["Y"].view(-1) >= 0) * 1.0
         )
 
@@ -125,7 +125,7 @@ class PredictorV1(BasicPredictor):
 
         # Y loss
         loss = self.criterion(pred_abs_factor, test_data_dict["Y"].view(-1).abs()) * 10
-        loss += self.binary_cross_entropy(
+        loss += self.binary_criterion(
             pred_sign_factor, (test_data_dict["Y"].view(-1) >= 0) * 1.0
         )
 
